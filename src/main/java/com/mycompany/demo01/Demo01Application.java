1972e7f70739435d7c694a812af934921b7e406a
sugnanags <sugnanags@gmail.com>
2026-07-04 01:35:21 +0530

    2nd Commit 4July2026

--- src/main/java/com/mycompany/demo01/Demo01Application.java
+++ src/main/java/com/mycompany/demo01/Demo01Application.java
@@ -9,7 +9,7 @@ public class Demo01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
-		System.out.println("Demo01Application : " + LocalTime.now())
+		System.out.println("Demo01Application : " + LocalTime.now());
 	}
 
 }
