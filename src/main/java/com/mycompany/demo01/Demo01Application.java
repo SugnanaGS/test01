531fe20574300929de595907f5bd4cd84aecdbcc
sugnanags <sugnanags@gmail.com>
2026-07-04 01:34:04 +0530

    1st Commit 4July2026

--- src/main/java/com/mycompany/demo01/Demo01Application.java
+++ src/main/java/com/mycompany/demo01/Demo01Application.java
@@ -1,5 +1,6 @@
 package com.mycompany.demo01;
 
+import java.util.ti,e.*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class Demo01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
+		System.out.println("Demo01Application : " + LocalTime.now())
 	}
 
 }
